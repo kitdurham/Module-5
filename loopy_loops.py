7cddd007ff18d6068be0450a613e9eb4797777a2
kitdurham <123882303+kitdurham@users.noreply.github.com>
2023-03-01 00:19:01 -0800

    Update loopy_loops.py

--- loopy_loops.py
+++ loopy_loops.py
@@ -1,1 +1,13 @@
+# HOMEWORK MODULE 5
 
+# Create a tuple named pokemon with the three strings as its elements 
+pokemon = ('picachu', 'charmander', 'bulbasaur')
+
+# Using index notation, print() the string that located at index 1 in pokemon
+print(pokemon[1])  # prints 'charmander'
+
+# Unpack the values of pokemon into three new variables with names starter1, starter2, starter3. Print each variable using the print() function to verify the unpacking.
+starter1, starter2, starter3 = pokemon
+print(starter1)  # prints 'picachu'
+print(starter2)  # prints 'charmander'
+print(starter3)  # prints 'bulbasaur'
