2ce71db83fe4679903dccca2a5ac0535b66cb707
kitdurham <123882303+kitdurham@users.noreply.github.com>
2023-03-01 04:00:42 -0800

    Update dynomite_dicts.py

--- dynomite_dicts.py
+++ dynomite_dicts.py
@@ -1,1 +1,18 @@
+# HOMEWORK 5 PART 3
 
+# Create an empty dictionary called pokedex
+pokedex = {}
+
+# Add key value pairs to the dictionary
+pokedex['Venosaur'] = ['Grass', 'Poison']
+pokedex['Charizard'] = ['Fire', 'Flying']
+pokedex['Blastoise'] = ['Water']
+
+# Print the dictionary to verify
+print(pokedex)
+
+# Remove 'Blastoise' from the dictionary
+del pokedex['Blastoise']
+
+# Print the dictionary to verify removal of 'Blastoise'
+print(pokedex)
