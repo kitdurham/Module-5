bef913558d848e58aade559b1d355e3baf9421af
kitdurham <123882303+kitdurham@users.noreply.github.com>
2023-03-01 00:23:30 -0800

    Update loopy_loops.py

--- loopy_loops.py
+++ loopy_loops.py
@@ -1,13 +1,19 @@
 # HOMEWORK MODULE 5
 
-# Create a tuple named pokemon with the three strings as its elements 
-pokemon = ('picachu', 'charmander', 'bulbasaur')
-
-# Using index notation, print() the string that located at index 1 in pokemon
 print(pokemon[1])  # prints 'charmander'
 
 # Unpack the values of pokemon into three new variables with names starter1, starter2, starter3. Print each variable using the print() function to verify the unpacking.
 starter1, starter2, starter3 = pokemon
-print(starter1)  # prints 'picachu'
-print(starter2)  # prints 'charmander'
-print(starter3)  # prints 'bulbasaur'
+print(starter1) 
+print(starter2) 
+print(starter3) 
+
+# Create a tuple using the tuple() built-in that contains the letters of your name. Print each character using the print() function to verify
+name_tuple = tuple('Sian')
+print(name_tuple)
+
+# Check if the character i is in your tuple representation of your name.
+if 'i' in name_tuple:
+    print('The character "i" is in the tuple')
+else:
+    print('The character "i" is not in the tuple')
