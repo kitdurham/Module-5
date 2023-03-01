2de715978c2ae131d5557c592f026efb6dbdc143
kitdurham <123882303+kitdurham@users.noreply.github.com>
2023-03-01 00:40:44 -0800

    Update listy_lo.py

--- listy_lo.py
+++ listy_lo.py
@@ -1,1 +1,29 @@
+# HOMEWORK MODULE 5 PART 2
+
+# Create a list named food with two elements 'rice' and 'beans'.
+
+
+# Append the string 'broccoli' to food using .append().
+
+
+# Add the strings 'bread' and 'pizza' to food using .extend().
+
+
+# Print the first two items in the food list using print() and slicing notation.
+
+
+# Print the last item in food using print() and index notation.
+
+
+# Create a list called breakfast from the string "eggs,fruit,orange juice" using the split() method.
+
+
+# Verify that breakfast has 3 elements using the len built-in.
+
+
+# prompts the user for a floating-point value until they enter stop. Store their entries in a list, and then find the average, min, and max of their entries and print them those values.
+
+
+
+
 
